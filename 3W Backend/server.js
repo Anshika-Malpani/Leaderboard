52932cd8d8ecdfd9a2899ded7e33c4f1d7e98d87
Anshika Malpani <138215307+Anshika-Malpani@users.noreply.github.com>
2025-07-18 22:47:30 +0530

    "Update CORS configuration to allow multiple origins and enable credentials."

--- 3W Backend/server.js
+++ 3W Backend/server.js
@@ -9,7 +9,8 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: ['http://localhost:5173', 'https://localhost:5173'],
+  credentials: true
 }));
 app.use(express.json());
 app.get('/', (req, res) => {
